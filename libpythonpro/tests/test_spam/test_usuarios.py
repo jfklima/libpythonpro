a93d400f40dcc2002fff8da147739ba5d73fa139
jfklima <jfranklincontatos@gmail.com>
2021-09-12 22:30:35 -0300

    Adicionado o conftest ao pacote test_spam.

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -1,23 +1,4 @@
-from libpythonpro.spam.db import Conexao
 from libpythonpro.spam.modelos import Usuario
-import pytest
-
-
-@pytest.fixture(scope='session')
-def conexao():
-    # Setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    # Tear Down
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
 
 
 def test_salvar_usuario(sessao):
@@ -31,3 +12,6 @@ def test_listar_usuarios(sessao):
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
+
+
+
